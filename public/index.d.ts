9dee85a0bf10437c8d51922627814aa7fa0e1ed5
Chendeli <419287484@qq.com>
2025-09-29 18:07:27 +0800

    更新

---
+++ public/index.d.ts
@@ -0,0 +1,13 @@
+/*
+ * @Author: 陈德立*******419287484@qq.com
+ * @Date: 2023-11-07 17:55:59
+ * @LastEditTime: 2025-09-29 18:05:03
+ * @LastEditors: 陈德立*******419287484@qq.com
+ * @Github: https://github.com/Alan1034
+ * @Description: 
+ * @FilePath: \ExcelUtils\public\index.d.ts
+ * 
+ */
+/// <reference types="vite/client" />
+
+declare module "excel-utils-bt";
